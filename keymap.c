6b9c1fa07005e241c0bd07563b6080cd4c73ac92
Daneryl <daneryl@gmail.com>
2019-07-31 16:56:29 +0200

    default led color white

--- keymap.c
+++ keymap.c
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,            KC_Q,    LT(2, KC_W),   KC_E,    KC_R,    KC_T,  KC_Y,    KC_U,    LT(2, KC_I),  KC_O,    KC_P,               KC_LBRC,     KC_RBRC,       KC_BSLS, KC_HOME, \
         LCTL_T(KC_ESC),    KC_A,    KC_S,          KC_D,    KC_F,    KC_G,  KC_H,    KC_J,    KC_K,         KC_L,    LCTL_T(KC_SCOLON),  KC_QUOTE,    KC_ENT,        KC_PGUP, \
         KC_LSPO,           KC_Z,    KC_X,          KC_C,    KC_V,    KC_B,  KC_N,    KC_M,    KC_COMM,      KC_DOT,  KC_SLSH,            KC_RSPC,                    KC_UP,   KC_PGDN, \
-        KC_LCTL,           KC_LALT, KC_LGUI,                                KC_SPC,                                  KC_RIGHT_PAREN,     MO(1),       KC_LEFT,       KC_DOWN, KC_RGHT  \
+        KC_LCTL,           KC_LALT, KC_LGUI,                                KC_SPC,                                  KC_RCBR,            MO(1),       KC_LEFT,       KC_DOWN, KC_RGHT  \
     ),
 
     [1] = LAYOUT(
