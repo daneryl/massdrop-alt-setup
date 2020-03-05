4631a6f5de689115910130cd515a9ad206c6bbe1
Daneryl <daneryl@gmail.com>
2020-03-05 12:11:28 +0100

    rgb effect for windows layout

--- keymap/keymap.c
+++ keymap/keymap.c
@@ -30,16 +30,16 @@ enum alt_keycodes {
 keymap_config_t keymap_config;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    // MAC 
+    // mac (main) 
     [0] = LAYOUT(
-        KC_ESC,            KC_1,    KC_2,          KC_3,    KC_4,    KC_5,  KC_6,    KC_7,    KC_8,         KC_9,    KC_0,               KC_MINS,     KC_EQUAL,      KC_BSPC, KC_GRV,  \
-        KC_TAB,            KC_Q,    LT(3, KC_W),   KC_E,    KC_R,    KC_T,  KC_Y,    KC_U,    LT(3, KC_I),  KC_O,    KC_P,               KC_LBRC,     KC_RBRC,       KC_BSLS, KC_HOME, \
-        LCTL_T(KC_ESC),    KC_A,    KC_S,          KC_D,    KC_F,    KC_G,  KC_H,    KC_J,    KC_K,         KC_L,    LCTL_T(KC_SCOLON),  KC_QUOTE,    KC_ENT,        KC_PGUP, \
-        KC_LSPO,           KC_Z,    KC_X,          KC_C,    KC_V,    KC_B,  KC_N,    KC_M,    KC_COMM,      KC_DOT,  KC_SLSH,            KC_RSPC,                    KC_UP,   KC_PGDN, \
-        KC_LCTL,           KC_LALT, KC_LGUI,                                KC_SPC,                                  KC_RCBR,            MO(2),       KC_LEFT,       KC_DOWN, KC_RGHT  \
+        KC_ESC,         KC_1,    KC_2,        KC_3, KC_4, KC_5, KC_6,          KC_7, KC_8,        KC_9,   KC_0,              KC_MINS,  KC_EQUAL, KC_BSPC, KC_GRV,  \
+        LT(4, KC_TAB),  KC_Q,    LT(3, KC_W), KC_E, KC_R, KC_T, KC_Y,          KC_U, LT(3, KC_I), KC_O,   KC_P,              KC_LBRC,  KC_RBRC,  KC_BSLS, KC_HOME, \
+        LCTL_T(KC_ESC), KC_A,    KC_S,        KC_D, KC_F, KC_G, KC_H,          KC_J, KC_K,        KC_L,   LCTL_T(KC_SCOLON), KC_QUOTE, KC_ENT,   KC_PGUP, \
+        KC_LSPO,        KC_Z,    KC_X,        KC_C, KC_V, KC_B, KC_N,          KC_M, KC_COMM,     KC_DOT, KC_SLSH,           KC_RSPC,            KC_UP,   KC_PGDN, \
+        KC_LCTL,        KC_LALT, KC_LGUI,                       LT(4, KC_SPC),                            KC_RALT,           MO(2),    KC_LEFT,  KC_DOWN, KC_RGHT  \
     ),
 
-    // Windows
+    // Gaming
     [1] = LAYOUT(
         KC_ESC,         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  \
         KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME, \
@@ -48,23 +48,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,        KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(2),   KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
 
+    //FN
     [2] = LAYOUT(
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_MUTE, \
+        TG(1)  , KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_MUTE, \
         _______, RGB_SPD, RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, _______, U_T_AUTO,U_T_AGCR,_______, KC_PSCR, KC_SLCK, KC_PAUS, _______, KC_END, \
         _______, RGB_RMOD,RGB_VAD, RGB_MOD, RGB_HUD, RGB_SAD, _______, _______, _______, _______, _______, _______,          _______, KC_VOLU, \
         _______, RGB_TOG, _______, _______, _______, MD_BOOT, TG_NKRO, DBG_TOG, _______, _______, _______, _______,          KC_PGUP, KC_VOLD, \
-        DF(1)  , DF(0)  , _______,                            _______,                            _______, _______, KC_HOME, KC_PGDN, KC_END  \
+        _______, _______, _______,                            _______,                            _______, _______, KC_HOME, KC_PGDN, KC_END  \
     ),
     
-    // tmux layout ?
+    //tmux layout 
     [3] = LAYOUT(
-        KC_TRNS, KC_TRNS,    KC_TRNS,          KC_TRNS,        KC_TRNS, TMUX_HSPLIT,   TMUX_VSPLIT,     KC_TRNS,      KC_TRNS,      KC_TRNS,    KC_TRNS,        KC_TRNS,              KC_TRNS, TMUX_CLEAR, KC_TRNS, \
-        KC_TRNS, KC_TRNS,    KC_TRNS,          KC_TRNS,        KC_TRNS, KC_TRNS,       KC_TRNS,         KC_TRNS,      KC_TRNS,      KC_TRNS,    KC_TRNS,        TMUX_COPY_MODE,       KC_TRNS, KC_TRNS,    KC_TRNS, \
-        KC_TRNS, KC_TRNS,    TMUX_SESSIONS,    TMUX_DETACH,    KC_TRNS, KC_TRNS,       TMUX_LEFT,       TMUX_DOWN,    TMUX_UP,      TMUX_RIGHT, TMUX_CMD_MODE,  KC_TRNS,                       KC_TRNS,    KC_TRNS, \
-        KC_TRNS, TMUX_ZOOM,  TMUX_KILL_PANE,   KC_TRNS,        KC_TRNS, KC_TRNS,       TMUX_NEXT_WINDOW,KC_TRNS,      KC_TRNS,      KC_TRNS,    KC_TRNS,        KC_TRNS,                       KC_TRNS,    KC_TRNS, \
-        KC_TRNS, KC_ESC,     KC_TRNS,                                                  KC_TRNS,                                             KC_TRNS,        KC_TRNS,              KC_TRNS, KC_TRNS,    KC_TRNS  \
+        KC_TRNS, KC_TRNS,   KC_TRNS,        KC_TRNS,     KC_TRNS, TMUX_HSPLIT, TMUX_VSPLIT,      KC_TRNS,   KC_TRNS, KC_TRNS,    KC_TRNS,       KC_TRNS,        KC_TRNS, TMUX_CLEAR, KC_TRNS, \
+        KC_TRNS, KC_TRNS,   KC_TRNS,        KC_TRNS,     KC_TRNS, KC_TRNS,     KC_TRNS,          KC_TRNS,   KC_TRNS, KC_TRNS,    KC_TRNS,       TMUX_COPY_MODE, KC_TRNS, KC_TRNS,    KC_TRNS, \
+        KC_TRNS, KC_TRNS,   TMUX_SESSIONS,  TMUX_DETACH, KC_TRNS, KC_TRNS,     TMUX_LEFT,        TMUX_DOWN, TMUX_UP, TMUX_RIGHT, TMUX_CMD_MODE, KC_TRNS,                 KC_TRNS,    KC_TRNS, \
+        KC_TRNS, TMUX_ZOOM, TMUX_KILL_PANE, KC_TRNS,     KC_TRNS, KC_TRNS,     TMUX_NEXT_WINDOW, KC_TRNS,   KC_TRNS, KC_TRNS,    KC_TRNS,       KC_TRNS,                 KC_TRNS,    KC_TRNS, \
+        KC_TRNS, KC_ESC,    KC_TRNS,                                           KC_TRNS,                                          KC_TRNS,       KC_TRNS,        KC_TRNS, KC_TRNS,    KC_TRNS  \
     ),
-   
+
+    //nav/util/vim-like layout
+    [4] = LAYOUT(
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, \
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, LALT(KC_LEFT), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, \
+        KC_TRNS, KC_TRNS, KC_TRNS,                                  KC_TRNS,                             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS  \
+    ),
+
     /*
     [X] = LAYOUT(
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
@@ -79,11 +89,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* #define RGB_MATRIX_STARTUP_MODE 2 */
 // Runs just one time when the keyboard initializes.
 void matrix_init_user(void) {
-  rgblight_mode(RGB_MATRIX_EFFECT_MAX-1);
 };
+
+void keyboard_post_init_user(void) {
+  debug_enable=true;
+  debug_matrix=true;
+  rgblight_mode(RGB_MATRIX_EFFECT_MAX-1);
+  rgb_matrix_set_flags(LED_FLAG_KEYLIGHT);
+}
 
 // Runs constantly in the background, in a loop.
 void matrix_scan_user(void) {
+  static uint8_t old_layer = 255;
+  uint8_t new_layer = biton32(layer_state);
+
+  if (old_layer != new_layer) {
+    switch (new_layer) {
+      case 0:
+        rgblight_mode(RGB_MATRIX_EFFECT_MAX-1);
+        break;
+      case 1:
+        rgblight_mode(RGB_MATRIX_EFFECT_MAX-2);
+        break;
+    }
+
+    old_layer = new_layer;
+  }
 };
 
 #define MODS_SHIFT  (get_mods() & MOD_BIT(KC_LSHIFT) || get_mods() & MOD_BIT(KC_RSHIFT))
